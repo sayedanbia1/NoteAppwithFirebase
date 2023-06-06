71a719e1f0bb6243197472eb06ffb689e6e9755f
sayedanbia1 <sayedanbia5555@gamil.com>
2023-06-06 21:57:29 +0430

    start project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'HomeScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NoteApp',
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
+    );
+  }
+}
