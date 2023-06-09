30cdd7774c3cef1f93a0eee7810e82704bbef9cd
sayedanbia1 <sayedanbia5555@gamil.com>
2023-06-09 17:09:38 +0430

    add sizedbox

--- lib/widget/note_card.dart
+++ lib/widget/note_card.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:note_app/styles/App_style.dart';
 
-  Widget notecard(Function()? ontap,QueryDocumentSnapshot doc){
+  Widget notecard(Function()? ontap, QueryDocumentSnapshot doc){
     return InkWell(
       onTap: ontap,
       child: Container(
@@ -15,10 +15,19 @@ import 'package:note_app/styles/App_style.dart';
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(doc["note_title"],style: appstyle.maintitle,),
-            Text(doc["creation_date"],style: appstyle.datatitle,),
-            Text(doc["note_content"],style: appstyle.maincontent,),
-
+            Text(
+              doc["note_title"],
+              style: appstyle.maintitle,),
+            SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              doc["creation_date"],style: appstyle.datatitle,),
+            SizedBox(
+              height: 8.0,
+            ),
+            Text(
+              doc["note_content"],style: appstyle.maincontent,),
           ],
         ),
       ),
